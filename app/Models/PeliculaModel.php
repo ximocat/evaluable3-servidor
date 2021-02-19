87bc119842c763ffb31d6e04bb8b1a305659e7dd
ximocat <ximocat@gmail.com>
2021-02-19 19:18:19 +0100

    Corregido error PeliculaModel

--- app/Models/PeliculaModel.php
+++ app/Models/PeliculaModel.php
@@ -8,11 +8,9 @@ class PeliculaModel extends Model
     protected $primaryKey = 'id';
     protected $allowedFields = ['titulo', 'anyo', 'duracion'];
     
-    /**
-     * Obtenemos un único equipo
-     */
+    //Metodo que devuelve una única peli cuyo id se pasa por parámetro
     public function get($id){     
-        $sql = "SELECT * FROM `equipos` WHERE equipos.id=:id:";
+        $sql = "SELECT * FROM 'peliculas' WHERE peliculas.id=:id:";
         $query = $this->query($sql, [
                 'id'     => $id,               
         ]);    
