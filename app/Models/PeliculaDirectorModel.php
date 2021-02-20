169b6d74330728eb5b9e347c1c196dce0db21182
ximocat <ximocat@gmail.com>
2021-02-20 23:14:52 +0100

    Añadida funcion get

--- app/Models/PeliculaDirectorModel.php
+++ app/Models/PeliculaDirectorModel.php
@@ -7,5 +7,13 @@ class PeliculaDirectorModel extends Model
     protected $table = 'peliculas_directores';
     protected $primaryKey = 'id_pelicula';
     protected $allowedFields = ['id_director'];
+
+    public function get($id){     
+        $sql = "SELECT id_director FROM peliculas_directores WHERE id_pelicula=:id:";
+        $query = $this->query($sql, [
+                'id'     => $id,               
+        ]);    
+        return $query->getResult('array');
+    }
     
 }
