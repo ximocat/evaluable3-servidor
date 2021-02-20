04d467092f4b1903ec41b9479cd81ef4d427a17f
ximocat <ximocat@gmail.com>
2021-02-20 20:27:57 +0100

    Añadida funcion get

--- app/Models/PeliculaActorModel.php
+++ app/Models/PeliculaActorModel.php
@@ -7,5 +7,13 @@ class PeliculaActorModel extends Model
     protected $table = 'peliculas_actores';
     protected $primaryKey = 'id_pelicula';
     protected $allowedFields = ['id_actor'];
+
+    public function get($id){     
+        $sql = "SELECT id_actor FROM peliculas_actores WHERE id_pelicula=:id:";
+        $query = $this->query($sql, [
+                'id'     => $id,               
+        ]);    
+        return $query->getResult('array');
+    }
     
 }
