b87c288a5a25438e2034eacc09b3d342bb54ec2c
ximocat <ximocat@gmail.com>
2021-02-20 08:45:24 +0100

    Añadidos recursos actor y director a routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,6 +34,8 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 //$routes->get('/', 'Home::index');
 $routes->resource('pelicula');
+$routes->resource('director');
+$routes->resource('actor');
 
 /*
  * --------------------------------------------------------------------
